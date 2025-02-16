f4b8035b36ba050e0e242e7cdb8511d96d895f73
hyeon <ski0123@naver.com>
2025-02-16 13:45:46 +0900

    first commit

---
+++ src/main/java/com/example/scheduledevelopsolution/ScheduleDevelopSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduledevelopsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleDevelopSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleDevelopSolutionApplication.class, args);
+    }
+
+}
